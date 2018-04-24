df0b14de9e2bf377d01a0b4c61bd4b8e24994d19
Jeffrey Loyola <JLyleLoyola528@24dcaea7d349>
2018-04-24 15:38:04 +0000

    converted about me and register pages to templates that extend base.html

--- flask_app.py
+++ flask_app.py
@@ -13,7 +13,7 @@ def hello_world():
 
 @app.route('/about_me')
 def about_me():
-    return app.send_static_file('about_me.html')
+    return render_template('about_me.html')
 
 '''
 @app.route('/class_schedule')
@@ -27,4 +27,4 @@ def class_schedule():
 
 @app.route('/register')
 def register():
-    return app.send_static_file('register.html')
+    return render_template('register.html')
