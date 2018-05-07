6aba56bf7dcf2e1da51c13cfa10edb9a2c0a0d45
Jeffrey Loyola <JLyleLoyola528@24dcaea7d349>
2018-05-07 15:36:53 +0000

    define the cousre table for database

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = (
         'mysql://JLyleLoyola528:'
         'ilovefood123'
-        '@<username>.mysql.pythonanywhere-services.com/'
-        '<username>$mysitedb')
+        '@JLyleLoyla528.mysql.pythonanywhere-services.com/'
+        'JLyleLoyola528$mysitedb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
