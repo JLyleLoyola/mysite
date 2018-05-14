e93c3b57d4fe21367cadd34ac1d5ae80ca6ee08c
Jeffrey Loyola <JLyleLoyola528@24dcaea7d349>
2018-05-14 16:08:29 +0000

    switched to using Flask-WTF for forms

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class BaseConfig(object):
         '@JLyleLoyola528.mysql.pythonanywhere-services.com/'
         'JLyleLoyola528$mysitedb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'eifheifjgifjeihfsdnvbncxkahefeigjiehf'
--- flask_app.py
+++ flask_app.py
@@ -5,11 +5,18 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View
+from flask_sslify import SSLify
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
 Bootstrap(app)
+SSLify(app)
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     period = db.Column(db.Integer)
@@ -24,8 +31,17 @@ class Song(db.Model):
     artist_name = db.Column(db.String(80))
     youtube_url = db.Column(db.String(300))
 
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        'Username', validators=[InputRequired(), Length(min=4, max=15)])
+    email = StringField(
+        'Email', validators=[InputRequired(), Email()])
+    password = PasswordField(
+        'Password', validators=[InputRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Register')
+
 @app.route('/')
-def homepage():
+def index():
     return render_template('index.html')
 
 @app.route('/about_me')
@@ -38,15 +54,35 @@ def class_schedule():
     return render_template('class_schedule.html',
                            courses=courses)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return (
+            form.username.data + ', ' +
+            form.email.data + ', ' +
+            form.password.data)
+    return render_template('register.html', form=form)
 
 @app.route('/top_ten_songs')
 def top_ten_songs():
     songs = Song.query.all()
     return render_template('top_ten_songs.html',
                            songs=songs)
+
+nav = Nav(app)
+@nav.navigation('mysite_navbar')
+def create_navbar():
+    home_view = View('Home', 'index')
+    register_view = View('Register', 'register')
+    about_me_view = View('About Me', 'about_me')
+    class_schedule_view = View('Class Schedule', 'class_schedule')
+    top_ten_songs_view = View('Top Ten Songs', 'top_ten_songs')
+    misc_subgroup = Subgroup('Misc',
+                             about_me_view,
+                             class_schedule_view,
+                             top_ten_songs_view)
+    return Navbar('MySite', home_view, misc_subgroup, register_view)
 
 if __name__ == '__main__':
   db.create_all()
